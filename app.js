85a5413feec9bd18982dab0f7bf551e67074a428
martignj <martigny.julien@grenoble-inp.org>
2024-04-22 22:49:01 +0200

    test

---
+++ app.js
@@ -0,0 +1,11 @@
+let version = "1.0"
+const API = "https://wakfu.cdn.ankama.com/gamedata/"
+
+async function updateVersion() {
+    version = await fetch('${API}/config.json')
+    console.log(version)
+}
+
+window.addEventListener("load", function(){ 
+     updateVersion()
+    });
