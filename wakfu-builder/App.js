37ae81920681a5fb0fa59e726a3a2356f36d4329
martignj <martigny.julien@grenoble-inp.org>
2024-04-23 19:01:00 +0200

    début d'exploitation des json

--- wakfu-builder/App.js
+++ wakfu-builder/App.js
@@ -1,19 +1,29 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
-//import jsonVersion from './db/files/version.json';
+import { Button, TextInput } from 'react-native-paper';
+import jsonVersion from './db/files/version.json';
+import jsonItems from './db/files/items.json';
+import jsonActions from './db/files/actions.json';
+import jsonItemsProps from './db/files/itemProperties.json';
+import jsonEquipItemTypes from './db/files/equipmentItemTypes.json';
+
 
 export default function App() {
+  const [text, setText] = useState("");
 
-  function updateDB() {
-   /* const shell = require('shelljs')
-    shell.echo('hi')*/
-  };
-
+  function displayItems(x) {
+    for (var item of jsonItems) {
+      {
+        console.log(item.title.fr)
+      }
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Button onPress={() => updateDB()}>Mettre à jour</Button>
+      <TextInput value={text} onChangeText={text => setText(text)}></TextInput>
+      <Button onPress={() => displayItems(text)}>Rechercher</Button>
       <StatusBar style="auto" />
     </View>
   );
